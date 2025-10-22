9d98649fc1c98797625aca8e7520263d4dfab149
Paraluman Mariel Orara <paralumanmarielorara@gmail.com>
2025-10-22 15:09:00 +0000

    initial models

---
+++ smartpark/model/ParkingLot.java
@@ -0,0 +1,6 @@
+public class ParkingLot{
+    public long lotId;
+    public String location;
+    public int capacity;
+    public int occupiedSpaces;
+}
---
+++ smartpark/model/Vehicle.java
@@ -0,0 +1,7 @@
+package smartpark.model.VehicleType;
+
+public class Vehicle{
+    public String licensePlate;
+    public VehicleType vehicleType;
+    public String ownerName;
+}
